88fd7b7225e791d0401cd56f71430b718e8f3b98
lihao <17326886995@163.com>
2018-11-24 18:10:31 +0800

    // default 控制器

---
+++ app/Http/Controllers/Admin/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DefaultController extends Controller
+{
+    //
+    public function test(){
+        echo 111;
+    }
+}
